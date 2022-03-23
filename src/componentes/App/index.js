eda5eec0ee5d4a68d8ce6032a3ea156756140e4f
Wallyson <wallysom2@gmail.com>
2022-03-23 08:37:46 -0400

    Pagina inicial

---
+++ src/componentes/App/index.js
@@ -0,0 +1,15 @@
+import Inicio from "../Inicio";
+import Horario from "../Horario";
+
+import "./style.css";
+
+function App() {
+  return (
+    <>
+      <Inicio />
+      <Horario />
+    </>
+  )
+}
+
+export default App;
